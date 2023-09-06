ecdba9ccbe118909b2a30574f6ba3cc4fe093062
MikhaelKreslavskii <116979503+MikhaelKreslavskii@users.noreply.github.com>
2023-09-06 13:58:16 +0300

    Delete task_2 from step_0.dart

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,35 +1,3 @@
-extension ParserLinks on String
-{
-  List<String> parseLink()
-  {
-    List<String> tokens = this.split(' ');
-    List<String> result =[];
-
-    for( int i=0;i<tokens.length;i++)
-    {
-      if(tokens[i].contains(RegExp(r'([a-z]+\.[a-z]+)')))
-      {
-        if(tokens[i].endsWith(','))
-        {
-        tokens[i] = tokens[i].replaceAll(',', '');
-          if(i<tokens.length-1)
-          {
-            tokens[i+1]= ', '+ tokens[i+1];
-          }
-        result.add("Link ('${tokens[i]} ')");
-        }
-      }
-      else
-      {
-        result.add("Text ('${tokens[i]} ')");
-      }
-    } 
-
-      
-    
-    return result;
-  }
-}
 
 void main() {
   // Implement an extension on [String], parsing links from a text.
@@ -38,7 +6,6 @@ void main() {
   // - `Hello, google.com, yay` ->
   //   [Text('Hello, '), Link('google.com'), Text(', yay')].
 
-  ///не учитывает запятые  
-  String text = "Hello, google.com, yay lfsdlfjd, rtrtr gogle.com";
-  print(text.parseLink());
+  
+  
 }
